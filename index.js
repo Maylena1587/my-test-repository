82b3388b9a4e51429bf83b8fbb6501217f0a6db4
Maylena Lorentes <lorentes@ukr.net>
2020-11-27 10:39:38 +0200

    вносит правки

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let one = " ";
- const n = " ";
+let n = " ";
 const totalLength = 5;
 const totalHight = 5;
-for(i = 0; i == totalHight/2; i++) {
+for (i = 0; i <= totalHight / 2; i++) {
     one += "1";
+    
     console.log(one);
 }
 
+for (a = 0; (a > totalHight / 2)+1; a++) {
+    n += "0";
+    
+    console.log(n);
+}
 // rhombus
